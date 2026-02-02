fb6daa5036cb738f0f631c246a6e0f07c95bd3ad
micbuffa <micbuffa@gmail.com>
2026-02-02 13:06:57 +0100

    snake

--- 3-Arrival/sketch.js
+++ 3-Arrival/sketch.js
@@ -1,12 +1,8 @@
-let nbVehicules = 20;
+let nbVehicules = 1;
 let target;
 let vehicle;
 let vehicles = [];
-// Texte
-let font;
-let points = [];
-// mode (snake ou text)
-let mode = "snake";
+
 
 // Appelée avant de démarrer l'animation
 function preload() {
@@ -19,20 +15,10 @@ function setup() {
 
   // On crée un véhicule à la position (100, 100)
   vehicle = new Vehicle(100, 100);
+  vehicles.push(vehicle);
 
   // La cible, ce sera la position de la souris
   target = createVector(random(width), random(height));
-
-  // Texte qu'on affiche avec textToPoint
-  // Get the point array.
-  points = font.textToPoints('Hello!', 350, 250, 305, { sampleFactor: 0.03 });
-
-  // on cree des vehicules, autant que de points
-  nbVehicules = points.length;
-  for (let i = 0; i < nbVehicules; i++) {
-    let v = new Vehicle(random(width), random(height));
-    vehicles.push(v);
-  }
 
 }
 
@@ -43,18 +29,6 @@ function draw() {
   // pour effet psychedelique
   //background(0, 0, 0, 10);
 
-  // On affiche le texte avec des cercles définis par le tableau points
-  points.forEach(pt => {
-    push();
-    fill("grey");
-    noStroke();
-    circle(pt.x, pt.y, 15);
-    pop();
-  });
-
-  switch (mode) {
-    case "snake":
- // Cible qui suit la souris, cercle rouge de rayon 32
   target.x = mouseX;
   target.y = mouseY;
 
@@ -84,19 +58,6 @@ function draw() {
     vehicle.update();
     vehicle.show();
   })
-      break;
-    case "text":
-      vehicles.forEach((vehicle, index) => {
-        // chaque véhicule vise un point du texte
-        let pt = points[index];
-        let target = createVector(pt.x, pt.y);
-        let steeringForce = vehicle.arrive(target);
-        vehicle.applyForce(steeringForce);
-        vehicle.update();
-        vehicle.show();
-      });
-      break;
-  }
  
 }
 
@@ -104,11 +65,5 @@ function draw() {
 function keyPressed() {
   if (key === 'd') {
     Vehicle.debug = !Vehicle.debug;
-  } else if (key === 's') {
-    // Mode = Snake
-    mode = "snake";
-  } else if (key === 't') {
-    // Mode = Text
-    mode = "text";
-  }
+  } 
 }
